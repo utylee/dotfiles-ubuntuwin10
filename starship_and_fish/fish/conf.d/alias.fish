e7b2e1d12dbe516cc045f9d3725d66b2568d8205
utylee <utylee@gmail.com>
2021-10-21 13:25:10 +0900

    fish alias added

--- starship_and_fish/fish/conf.d/alias.fish
+++ starship_and_fish/fish/conf.d/alias.fish
@@ -7,3 +7,12 @@ function dt
 end
 
 alias td="dt"
+alias t1="source ~/.tmuxset-misc.fish"
+alias t0="source ~/.tmuxset-blog.fish"
+alias we='curl http://192.168.0.202:9010;echo -e "\n"'
+
+alias wsl2="tmux rename-window 'wsl2';ssh -p 8824 utylee@192.168.0.204"
+alias mac="tmux rename-window 'mac';ssh -p 8817 utylee@192.168.0.107"
+alias od="tmux rename-window 'od';ssh -p 8821 odroid@192.168.0.201"
+alias hc="tmux rename-window 'hc';ssh -p 8822 odroid@192.168.0.202"
+alias hc2="tmux rename-window 'hc2';ssh -p 8823 odroid@192.168.0.203"
