462afb77021dea2e5a3ddc5eda1dd3f48d2cc9f2
utylee <utylee@gmail.com>
2021-10-23 14:30:18 +0900

    fzf default to fd from rg

--- starship_and_fish/fish/config.fish
+++ starship_and_fish/fish/config.fish
@@ -19,13 +19,18 @@ set -gx CXX $CLANGHOME/bin/clang++
 set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH $CLANGHOME/lib 
 
 # FZF
-set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --follow --no-ignore"
+set -gx FZF_DEFAULT_COMMAND 'fd --type file --color=always --follow --hidden --exclude .git'
+set -gx FZF_DEFAULT_OPTS "--ansi"
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_ALT_C_COMMAND "fd --type d --color=always"
+#
+#set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --follow --no-ignore"
 #set -gx FZF_CTRL_T_COMMAND "rg --files --hidden --follow --no-ignore"
-set -gx FZF_CTRL_T_COMMAND "find . -depth"
-set -gx FZF_ALT_C_COMMAND "find . -depth"
-#set -gx FZF_ALT_C_COMMAND "rg --hidden --files --null | xargs -0 dirname | uniq"
-#set -gx FZF_ALT_C_COMMAND "rg --hidden --sort-files --files --null 2> /dev/null | xargs -0 dirname | uniq"
-
+#set -gx FZF_CTRL_T_COMMAND "rg --files --hidden --follow --no-ignore"
+#set -gx FZF_ALT_C_COMMAND "find . -depth"
+##set -gx FZF_ALT_C_COMMAND "rg --hidden --files --null | xargs -0 dirname | uniq"
+##set -gx FZF_ALT_C_COMMAND "rg --hidden --sort-files --files --null 2> /dev/null | xargs -0 dirname | uniq"
+#
 # VENV
 set -gx WORKON_HOME $HOME/.virtualenvs
 
