932523c79f86716fb28e879e41c34259d0a35390
utylee <utylee@gmail.com>
2021-10-29 23:20:27 +0900

    updated, and making fuzzy Z(zz), fzf shortcut reorganize

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -43,6 +43,12 @@ function uv
     curl http://192.168.0.202:9202/vt
 end
 
+function bye
+	cd ~/utylee/goodbyedpi-0.1.5/x86_64/
+	./goodbyedpi.exe -1
+end
+
+
 alias td="dt"
 alias t1="source ~/.tmuxset-misc.fish"
 alias t0="source ~/.tmuxset-blog.fish"
@@ -53,3 +59,4 @@ alias wsl2="tmux rename-window 'wsl2';ssh -p 8824 utylee@192.168.0.204"
 alias od="tmux rename-window 'od';ssh -p 8821 odroid@192.168.0.201"
 alias hc="tmux rename-window 'hc';ssh -p 8822 odroid@192.168.0.202"
 alias hc2="tmux rename-window 'hc2';ssh -p 8823 odroid@192.168.0.203"
+alias mac="tmux rename-window 'mac';ssh -p 8817 utylee@192.168.0.107"
