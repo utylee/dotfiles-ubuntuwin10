9e0bdda1060280908b4e82853739f3605e98cd23
utylee <utylee@gmail.com>
2021-11-13 00:09:00 +0900

    config/fish/_fzf_search_git_log.fish: multi select function added

--- .config/fish/functions/_fzf_search_git_log.fish
+++ .config/fish/functions/_fzf_search_git_log.fish
@@ -10,14 +10,26 @@ function _fzf_search_git_log --description "Search the output of git log and pre
             _fzf_wrapper --ansi \
                 --tiebreak=index \
                 --preview='git show --color=always {1}' \
+				--multi \
                 --query=(commandline --current-token) \
                 $fzf_git_log_opts
         )
-        if test $status -eq 0
-            set abbreviated_commit_hash (string split --max 1 " " $selected_log_line)[1]
-            set commit_hash (git rev-parse $abbreviated_commit_hash)
-            commandline --current-token --replace $commit_hash
-        end
+
+		set paths
+		for path in $selected_log_line
+			if test $status -eq 0
+				#set abbreviated_commit_hash (string split --max 1 " " $selected_log_line)[1]
+				set abbreviated_commit_hash (string split --max 1 " " $path)[1]
+				set commit_hash (git rev-parse $abbreviated_commit_hash)
+				commandline --current-token --replace $commit_hash
+
+				set --append paths $commit_hash
+				#set --append paths $path
+				#set --append cleaned_paths (string sub --start=4 $path)
+			end
+		end
+
+        commandline --current-token --replace -- (string escape -- $paths | string join ' ')
     end
 
     commandline --function repaint
