175592ea60ca1153d4caeba72d00cfa889e2d2d9
utylee <utylee@gmail.com>
2022-09-13 20:07:27 +0900

    changed: fish function madd space processing for url

--- .config/fish/functions/madd.fish
+++ .config/fish/functions/madd.fish
@@ -6,14 +6,25 @@ function madd
 	# if not set -q argv[1]
 		echo '0: writing, 1:arranging, 2:mixing'
 	else
+		set b $argv[2]
+		set b (string replace -a '?' '_u_qa_' "$b")
+		set b (string replace -a '&' '_u_im_' "$b")
+		set b (string replace -a ' ' '_u_sp_' "$b")
+
 		if test $argv[1] -eq 0
-		    curl http://192.168.1.202:9007/add/writing/$argv[2]
+		    echo http://193.168.1.202:9007/add/writing/"$b"
+		    curl http://192.168.1.202:9007/add/writing/"$b"
+		    # curl http://192.168.1.202:9007/add/writing/$argv[2]
 			# curl http://utylee.duckdns.org/midea/add/writing/$argv[2]
 		else if test $argv[1] -eq 1
-		    curl http://192.168.1.202:9007/add/arranging/$argv[2]
+		    echo http://193.168.1.202:9007/add/arranging/"$b"
+		    curl http://192.168.1.202:9007/add/arranging/"$b"
+		    # curl http://192.168.1.202:9007/add/arranging/$argv[2]
 			# curl http://utylee.duckdns.org/midea/add/arranging/$argv[2]
 		else if test $argv[1] -eq 2
-		    curl http://192.168.1.202:9007/add/mixing/$argv[2]
+		    echo http://193.168.1.202:9007/add/mixing/"$b"
+		    curl http://192.168.1.202:9007/add/mixing/"$b"
+		    # curl http://192.168.1.202:9007/add/mixing/$argv[2]
 			# curl http://utylee.duckdns.org/midea/add/mixing/$argv[2]
 		end
 	end
