84359b43fd3616d5b5cbf10d73c1bdf896a06c6f
utylee <utylee@gmail.com>
2024-04-25 02:49:33 +0900

    chg: .config/fish/function/zz.fish bug fix

--- .config/fish/functions/zz.fish
+++ .config/fish/functions/zz.fish
@@ -3,7 +3,10 @@ function zz
 	# 인자가 있어서 함수를 실행합니다 
 	if ! test -z $argv
 		#cd (__z -l | fzf --filter=$argv | sed -n '1p' | string replace -a -r '[\d]*\s*/' '/')
-		set -l dir (__z -l | fzf --filter=$argv | sed -n '1p' | string replace -a -r '\d*[\.\d*]*\s*/' '/')
+		#set -l dir (__z -l | fzf --filter=$argv | sed -n '1p' | string replace -a -r '\d*[\.\d*]*\s*/' '/')
+		# 8001 디렉토리를 삭제시키는 버기가 있어 \s*를 \s+로 수정했습니다
+		set -l dir (__z -l | fzf --filter=$argv | sed -n '1p' | string replace -a -r '\d*[\.\d*]*\s+/' '/')
+		#echo $dir
 
 		# 검색 결과가 있어야 실행합니다
 		if ! test -z $dir
